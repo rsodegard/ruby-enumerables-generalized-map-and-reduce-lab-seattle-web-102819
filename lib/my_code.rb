b5f49568ba557f44492987f46bec1f7618dc82dc
Ryan Odegard <rsodegard@gmail.com>
2019-09-30 19:09:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do 
+    new_array.push(source_array[i * -1])
+    i =+ 1 
+  end 
+  new_array
+end
+
+
+def reduce(source_array, starting_value=0)
+
+end
