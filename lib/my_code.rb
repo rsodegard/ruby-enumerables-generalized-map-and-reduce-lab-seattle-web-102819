2bcca0ff3dc02e849c610c24c24ad9b9ca3951f7
Ryan Odegard <rsodegard@gmail.com>
2019-09-30 19:16:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   new_array = []
   i = 0 
   while i < source_array.length do 
-    new_array.push(source_array[i * -1])
+    new_array.push yield(source_array[i])
     i =+ 1 
   end 
   new_array
